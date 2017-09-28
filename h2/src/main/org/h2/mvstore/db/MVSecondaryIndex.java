9207122dde097f3a6caf6c4ef5d9a0bf765846e9
andrei <andrei.tokar@gmail.com>
2017-09-27 21:10:28 -0400

    merge hiccup

--- h2/src/main/org/h2/mvstore/db/MVSecondaryIndex.java
+++ h2/src/main/org/h2/mvstore/db/MVSecondaryIndex.java
@@ -7,7 +7,6 @@ package org.h2.mvstore.db;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
