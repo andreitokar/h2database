081c20c7e6a9d5e005f849efe9d24b93e48ad5bd
Andrei Tokar <andrei.tokar@gmail.com>
2020-02-16 18:31:42 -0500

    reuse of a traversal path on failed update attempts

--- h2/src/main/org/h2/mvstore/CursorPos.java
+++ h2/src/main/org/h2/mvstore/CursorPos.java
@@ -5,6 +5,8 @@
  */
 package org.h2.mvstore;
 
+import org.h2.mvstore.type.DataType;
+
 /**
  * A position in a cursor.
  * Instance represents a node in the linked list, which traces path
@@ -32,6 +34,8 @@ public final class CursorPos<K,V> {
     public CursorPos<K,V> parent;
 
 
+    private CursorPos() {}
+
     public CursorPos(Page<K,V> page, int index, CursorPos<K,V> parent) {
         this.page = page;
         this.index = index;
@@ -53,14 +57,88 @@ public final class CursorPos<K,V> {
     static <K,V> CursorPos<K,V> traverseDown(Page<K,V> page, K key) {
         CursorPos<K,V> cursorPos = null;
         while (!page.isLeaf()) {
-            int index = page.binarySearch(key) + 1;
-            if (index < 0) {
-                index = -index;
-            }
+            int index = page.binarySearch(key);
+            index = index < 0 ? ~index : index + 1;
             cursorPos = new CursorPos<>(page, index, cursorPos);
             page = page.getChildPage(index);
         }
         return new CursorPos<>(page, page.binarySearch(key), cursorPos);
+    }
+
+    /**
+     * Searches for a given key and creates a breadcrumb trail through a B-tree
+     * rooted at a given Page. Resulting path starts at "insertion point" for a
+     * given key and goes back to the root.
+     *
+     * @param page      root of the tree
+     * @param key       the key to search for
+     * @param lastPath  path of a previous traversal for the same key, that can be re-used
+     *                  to minimize object creation and/or number of key comparisons
+     * @return head of the CursorPos chain (insertion point)
+     */
+    static <K,V> CursorPos<K,V> traverseDown(Page<K,V> page, K key, CursorPos<K,V> lastPath) {
+        lastPath = reverse(lastPath, null);
+        DataType<K> comparator = page.map.getKeyType();
+        CursorPos<K,V> cursorPos = null;
+        boolean diverge = false;
+        while (true) {
+            if (lastPath == null) {
+                lastPath = new CursorPos<>();
+                diverge = true;
+            }
+            int index = lastPath.index;
+            boolean isLeaf = page.isLeaf();
+            if (!diverge) {
+                if (page == lastPath.page) {
+                    return reverse(lastPath, cursorPos);
+                }
+                assert lastPath.page.isLeaf() || index >= 0;
+                diverge = isLeaf != lastPath.page.isLeaf();
+                if (!diverge) {
+                    int keyCount = page.getKeyCount();
+                    if (!isLeaf) {
+                        diverge = index > keyCount ||
+                                index > 0 && comparator.compare(page.getKey(index - 1), key) > 0 ||
+                                index < keyCount && comparator.compare(key, page.getKey(index)) >= 0;
+
+                    } else if(index >= 0) {
+                        diverge = index >= keyCount || comparator.compare(page.getKey(index), key) != 0;
+                    } else {
+                        int indx = ~index;
+                        diverge = indx > keyCount ||
+                            indx > 0 && comparator.compare(page.getKey(indx - 1), key) >= 0 ||
+                            indx < keyCount && comparator.compare(key, page.getKey(indx)) >= 0;
+                    }
+                }
+            }
+            CursorPos<K, V> tmp = lastPath.parent;
+            lastPath.parent = cursorPos;
+            cursorPos = lastPath;
+            lastPath = tmp;
+
+            cursorPos.page = page;
+            if (diverge) {
+                index = page.binarySearch(key);
+                if (!isLeaf) {
+                    index = index < 0 ? ~index : index + 1;
+                }
+                cursorPos.index = index;
+            }
+            if (isLeaf) {
+                return cursorPos;
+            }
+            page = page.getChildPage(index);
+        }
+    }
+
+    private static <K,V> CursorPos<K,V> reverse(CursorPos<K,V> cursorPos, CursorPos<K,V> res) {
+        while (cursorPos != null) {
+            CursorPos<K, V> tmp = cursorPos.parent;
+            cursorPos.parent = res;
+            res = cursorPos;
+            cursorPos = tmp;
+        }
+        return res;
     }
 
     /**
